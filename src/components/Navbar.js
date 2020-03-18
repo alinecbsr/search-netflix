1e609af4fdd80877d023ceace9cd0af74a42e589
alinecbsr <alinecbsr@gmail.com>
2020-03-18 15:35:52 -0300

    Started Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import Netflix from '../assets/image/netflix-logo.svg';
+import Gift from '../assets/image/gift-icon.svg';
+import Bell from '../assets/image/bell-icon.svg';
+import User from '../assets/image/user-icon.svg';
 
 class Navbar extends Component {
   render() {
     return (
-      <header className="Navbar">
-        <h1>Navbar</h1>
-      </header>
+      <div className="main__header">
+        <div className="navbar">
+          <nav className="navbar__left">
+            <img
+              className="navbar__left--logo"
+              src={Netflix}
+              alt="Logo Netglix"
+            />
+            <ul className="navbar__left--list">
+              <li className="navbar__left--item">Trending</li>
+              <li className="navbar__left--item">Popular</li>
+              <li className="navbar__left--item">Top Rated</li>
+              <li className="navbar__left--item">Now</li>
+              <li className="navbar__left--item">Coming</li>
+            </ul>
+          </nav>
+          <div className="navbar__right">
+            <ul className="navbar__right--list">
+              <li className="navbar__right--item">KIDS</li>
+            </ul>
+            <img className="navbar__right--icon" src={Gift} alt="Gift icon" />
+            <img className="navbar__right--icon" src={Bell} alt="Bell icon" />
+            <img className="navbar__right--icon" src={User} alt="User icon" />
+          </div>
+        </div>
+      </div>
     );
   }
 }
